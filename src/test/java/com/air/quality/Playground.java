14c46ee033655af5dfac9abe1f55da42e422b27f
markov <amorpheus@abv.bg>
2020-12-28 13:19:54 +0000

    Minor update.

---
+++ src/test/java/com/air/quality/Playground.java
@@ -0,0 +1,15 @@
+package com.air.quality;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import java.util.Map;
+
+public class Playground {
+
+    @Test
+    @Ignore
+    public void test(){
+        Map<String, String> env = System.getenv();
+        System.out.println();
+    }
+}
